399f37e52d5cc8691ae3b42bb27e46f0eb5c30e3
Racheal <rachealnjugush@gmail.com>
2025-01-23 09:20:50 +0300

    first commit

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api
+from flask_cors import CORS
 from models import db
 from resources.products import ProductListResource, ProductResource
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app)
 
 db.init_app(app)
 migrate = Migrate(app, db)
